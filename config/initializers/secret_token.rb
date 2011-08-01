ed3446424d7458e25e27022ab5c748c852f8a39a
Daniel Dosen <ddosen@gmail.com>
2011-08-01 15:00:15 -0700

    initial commit - textmate free project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KitchenSink::Application.config.secret_token = '6df670abee2be057318ac73cb1329907705af264b6dcffd59cb02d5f5c508a200aea063b6eb14bd6480fd5aea18bd952bb663209479a24ae497d0f79afb323bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KitchenSink::Application.config.session_store :cookie_store, key: '_kitchen_sink_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KitchenSink::Application.config.session_store :active_record_store
